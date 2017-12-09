b370ab17d78441a871bde43b2ab83454272b7fa6
Black Tigers <blacktigersftc@gmail.com>
2017-12-09 17:24:28 +0200

    Try

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BT_AutoBlueRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BT_AutoBlueRight.java
@@ -103,7 +103,7 @@ public class BT_AutoBlueRight extends LinearOpMode {
         }
         telemetry.addData("Status", "Identified column: %s ",vuMark);
         telemetry.update();
-
+//Test
     robot.jewels.moveJewel(BT_Jewels.JewelColor.RED , this);
     }
 }
